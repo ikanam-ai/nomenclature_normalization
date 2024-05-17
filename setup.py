57e79d663f09594b3d44dfa0c5189f623e27b2cf
Chetand1228 <74776380+Chetoff1228@users.noreply.github.com>
2024-05-17 20:17:18 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Приведение номенклатуры участников рынка к официальному Классификатору Аметист Кэпитал Строительных Ресурсов',
+    author='ikanam 1218',
+    license='MIT',
+)
